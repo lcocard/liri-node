4317cd31d9bb5e95035144b3a379b94e21756bb0
Lawrence Cocard <12580746+lcocard@users.noreply.github.com>
2019-08-20 00:48:23 -0400

    add movie-this and do-what-it-says functions

--- liri.js
+++ liri.js
@@ -12,11 +12,6 @@ var commands = process.argv[2];
 var value = process.argv[3];
 
 switch (commands) {
-
-}
-
-
-switch (commands) {
     case "concert-this":
         concert(value);
         break;
@@ -27,7 +22,7 @@ switch (commands) {
         omdb(value);
         break;
     case "do-what-it-says":
-        doThis(value);
+        queryThis(value);
         break;
 };
 
@@ -36,17 +31,17 @@ function concert(value) {
         .then(function (response) {
             for (var i = 0; i < response.data.length; i++) {
                 var datetime = response.data[i].datetime;
-                var dateArr = datetime.split('T');
                 var concertResults =
                     "--------------------------------------------------------------------" +
                     "\nVenue Name: " + response.data[i].venue.name +
                     "\nVenue Location: " + response.data[i].venue.city +
-                    "\nDate of the Event: " + moment(dateArr[0]).format("MM/DD/YYYY");
+                    "\nDate of the Event: " + moment(datetime).format("MM/DD/YYYY");
+
                 console.log(concertResults);
             }
         })
         .catch(function (error) {
-            console.log(error);
+            console.log('Band/Artist not found : ' + value);
         });
 }
 
@@ -63,12 +58,58 @@ function spot(value) {
                     "--------------------------------------------------------------------" +
                     "\nArtist(s): " + response.tracks.items[i].artists[0].name +
                     "\nSong Name: " + response.tracks.items[i].name +
-                    "\nAlbum Name: " + response.tracks.items[i].album.name +
-                    "\nPreview Link: " + response.tracks.items[i].preview_url;
+                    "\nPreview Link: " + response.tracks.items[i].preview_url +
+                    "\nAlbum Name: " + response.tracks.items[i].album.name;
                 console.log(spotifyResults);
             }
         })
         .catch(function (err) {
-            console.log(err);
+            //console.log(err);
+            console.log('Song not found : ' + value);
         });
+}
+
+function omdb(value) {
+    if (!value) {
+        value = "mr nobody";
+    }
+    axios.get("https://www.omdbapi.com/?t=" + value + "&y=&plot=long&apikey=trilogy")
+        .then(function (response) {
+            var movieResults =
+                "--------------------------------------------------------------------" +
+                "\nMovie Title: " + response.data.Title +
+                "\nYear of Release: " + response.data.Year +
+                "\nIMDB Rating: " + response.data.imdbRating +
+                "\nRotten Tomatoes Rating: " + response.data.Ratings[1].Value +
+                "\nCountry Produced: " + response.data.Country +
+                "\nLanguage: " + response.data.Language +
+                "\nPlot: " + response.data.Plot +
+                "\nActors/Actresses: " + response.data.Actors;
+            console.log(movieResults);
+        })
+        .catch(function (error) {
+            //console.log(error);
+            console.log('Movie not found : ' + value);
+        });
+}
+
+function queryThis(value) {
+    fs.readFile("random.txt", "UTF8", (err, data) => {
+        if (err) throw err;
+        var queryValue = data.split(",")[1];
+        var queryCommands = data.split(",")[0];
+
+        switch (queryCommands) {
+            case "concert-this":
+                concert(queryValue);
+                break;
+            case "spotify-this-song":
+                spot(queryValue);
+                break;
+            case "movie-this":
+                omdb(queryValue);
+                break;
+        };
+
+    });
 }
